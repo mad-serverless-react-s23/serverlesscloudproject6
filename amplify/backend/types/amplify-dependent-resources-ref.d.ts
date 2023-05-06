0a32dd7bc398b333c572cf6ef8ebaec06b5167f1
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-05-05 21:31:48 -0500

    I think I found the problem
    damned typos

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "serverlesscloudproj692be8f46": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
